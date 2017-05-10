63d67415cd9310e140637ac6909d9f80b3aac62e
facusagua <facusagua@hotmail.com>
2017-05-09 22:37:25 -0300

    a

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ angular.module('mm', ['ionic', 'ngCordova', 'angular-md5', 'pascalprecht.transla
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
         if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-            window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+            window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             window.cordova.plugins.Keyboard.disableScroll(true);
         }
         if (window.StatusBar) {
